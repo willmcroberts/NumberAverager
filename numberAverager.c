7816a14e08c081b671998b70bdd664726e40c981
willmcroberts <williammcroberts15@gmail.com>
2025-01-24 19:38:10 +0000

    wrote the bulk of the averager

--- numberAverager.c
+++ numberAverager.c
@@ -4,10 +4,15 @@ int main() {
 
 	int arrayAmount;
 	int value;
+	float average;
+	int part;
+	int sum = 0;
 
 	printf("How many values are you entering?: ");
 	scanf("%d", &arrayAmount);
 	int values[arrayAmount];
+
+	// input values	
 
 	for (int i = 0; i < arrayAmount; i++) {
 		printf("Number %d: ", i + 1);
@@ -15,7 +20,11 @@ int main() {
 		values[i] = value;
 	} // end for loop
 	
-	printf("[ ");
+	printf(" \n");
+
+	// print values
+
+	printf("Numbers entered: [ ");
 
 	for (int i = 0; i < arrayAmount; i++) {
 
@@ -23,4 +32,13 @@ int main() {
 	} // end of for loop
 
 	printf("] \n");
+
+	for (int i = 0; i < arrayAmount; i++) {
+		part = values[i];
+		sum = sum + part;
+	} // end of for loop
+
+	average = sum / arrayAmount;
+
+	printf("Average: %f \n", average);
 } // end main
